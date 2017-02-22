923d77047da26c4c39bf9f98196679a67789c02c
James <gisjmr@gmail.com>
2017-02-22 22:22:10 +0000

    Added JS file

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.bg1').hide();
+  $('.bg1').fadeIn(3000);
+})
+
+
+// function main(){
+//   $('.skillset').hide();
+//   $('.skillset').fadeIn(1000);
+//   $('.projects').hide();
+//   $('.projects-button').on('click', function() {
+//     $(this).toggleClass('active');
+//     $(this).text('Projects Views');
+//     $(this).next().slideToggle(400);
+//
+//
+//   }
+//                           );
+// }
