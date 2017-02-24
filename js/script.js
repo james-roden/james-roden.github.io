dc4008b250a16ab7892bca8a6adcffc03f365b12
James <gisjmr@gmail.com>
2017-02-24 21:53:57 +0000

    Beautiful slideshow background added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-  $('.page1').hide();
-  $('.page1').fadeIn(1000);
   $('.scroll-button').click(function(){
     $('html, body').animate({
       scrollTop: $('.page2').offset().top
     }, 1000);
-  })})
+    })
+  })
