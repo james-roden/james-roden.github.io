8cbd1241a4c8bc5e45720e73fd10cb95218d9b41
James <gisjmr@gmail.com>
2017-06-25 21:08:25 +0100

    JS tweaks, cartography section added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
+  $('.name').animate({left: 0}, 2000);
+  $('.skills').animate({left: 0}, 2000);
   $('.scroll-button').click(function(){
     $('html, body').animate({
-      scrollTop: $('.page2').offset().top
-    }, 1000);
+      scrollTop: $('.page2').offset().top}, 1000);
     })
-  })
+  });
+
+
+
+$(window).scroll(function() {
+    var aboutHeight = $(".page2").outerHeight()
+    if ($(this).scrollTop()>aboutHeight)
+     {
+      $('.navbar').fadeIn();
+     }
+    else
+     {
+      $('.navbar').fadeOut();
+     }
+ });
