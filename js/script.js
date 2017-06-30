b1c469c3ed06477979df589b9e8844207de67277
James <gisjmr@gmail.com>
2017-06-30 20:17:58 +0100

    nav update

--- js/script.js
+++ js/script.js
@@ -5,10 +5,24 @@ $(document).ready(function(){
     $('html, body').animate({
       scrollTop: $('.about').offset().top}, 1000);
     })
+  $(".nav-about").click(function() {
+    $('html,body').animate({
+      scrollTop: $(".about").offset().top}, 'slow');
+  });
+
+  $(".nav-cartography").click(function() {
+    $('html,body').animate({
+      scrollTop: $(".cartography").offset().top}, 'slow');
+  });
+
+  $(".nav-projects").click(function() {
+    $('html,body').animate({
+      scrollTop: $(".github").offset().top}, 'slow');
+  });
   });
 
 $(window).scroll(function() {
-    var aboutHeight = $(".about").outerHeight()
+    var aboutHeight = $(".about").outerHeight();
     if ($(this).scrollTop()>aboutHeight)
      {
       $('.navbar').fadeIn();
