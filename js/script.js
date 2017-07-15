07211faf9b2280030c4a96c8cc87db3145cb4dbb
James <gisjmr@gmail.com>
2017-07-16 00:40:52 +0100

    UI updates and JS

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
       $('.navbar').fadeIn();
     }
     else {
-      $('.navbar').fadeOut();
+      //$('.navbar').fadeOut();
     }
   });
 
@@ -26,11 +26,13 @@ $(document).ready(function(){
   $(".nav-code").click(function() {
     $('html, body').animate({
       scrollTop: $("#code").offset().top - 100}, 1000);
+      TweenMax.staggerFrom("#code .content-card", 0.5, {x:-1000, scale:0.5, opacity:0, delay:0.2}, 0.2);
   });
 
   $(".nav-arcgis").click(function() {
     $('html, body').animate({
       scrollTop: $("#arcgis").offset().top - 100}, 1000);
+      TweenMax.staggerFrom("#arcgis .content-card", 0.5, {x:-1000, scale:0.5, opacity:0, delay:0.2}, 0.2);
   });
 
   $(".nav-webgis").click(function() {
@@ -41,5 +43,7 @@ $(document).ready(function(){
   $(".nav-cartography").click(function() {
     $('html, body').animate({
       scrollTop: $("#cartography").offset().top - 100}, 1000);
+      // TweenMax.from("#cartography .content-card", 0.5, {y:-1000, opacity:0, delay:0.2});
   });
+
 });
